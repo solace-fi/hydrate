3abd96380df79abef4671a5feef5130a2c455a6f
i001962@gmail.com <i001962@gmail.com>
2022-05-09 16:14:47 -0700

    bump vesion

--- test/hydrate.test.ts
+++ test/hydrate.test.ts
@@ -1,6 +1,6 @@
 import { hydrateLibrary, simulateSIP, metalog, listSIPs, p, q } from '../src/hydrate';
 import example_tokens from '../src/json/example_tokens.json';
-//import example_bounded from '../src/json/example_bounded.json'
+import example_bounded from '../src/json/example_bounded.json'
 // import example_gaussian from '../src/json/example_gaussian.json'
 
 describe('SolaceBalance', () => {
@@ -46,8 +46,10 @@ describe('SolaceBalance', () => {
     describe('#q', () => {
         it('will return a valid response', async () => {
             const p = [0.9, 0.5, 0.1] // variable represents the probability values to find quantiles for
-            const a = example_tokens.sips[0].arguments.aCoefficients
-            console.log(q(p, a, undefined, undefined))
+            const a = example_bounded.sips[0].arguments.aCoefficients
+            const bl = example_bounded.sips[0].arguments.lowerBound
+            const bu = example_bounded.sips[3].arguments.upperBound
+            console.log(q(p, a, bl, bu))
         })
     })
 })
